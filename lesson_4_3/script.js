0a9379984a12d4bd7fe1132a900f891eaefd146f
Honcharenko Danylo <danilo.gonch@gmail.com>
2025-10-03 19:46:07 +0300

    refactor

--- lesson_4_3/script.js
+++ lesson_4_3/script.js
@@ -7,21 +7,23 @@ if (cityOfResidence === null) alert("Шкода, що Ви не захотіли
 const favoriteSport = prompt("Улюблений вид спорту");
 if (favoriteSport === null) alert("Шкода, що Ви не захотіли ввести свій вид спорту");
 
-const capitalAndCountry = new Map();
-capitalAndCountry.set("Київ", "України");
-capitalAndCountry.set("Вашингтон", "USA");
-capitalAndCountry.set("Лондон", "Англії");
+const capitalAndCountry = [
+    {capital: "Київ", country: "України"},
+    {capital: "Вашингтон", country: "USA"},
+    {capital: "Лондон", country: "Англії"}
+];
 
-const sportAndChampion = new Map();
-sportAndChampion.set("Футбол", "Ліонель Мессі");
-sportAndChampion.set("Формула-1", "Макс Ферстаппен");
-sportAndChampion.set("Бокс", "Олександр Усик");
+const sportAndChampion = [
+    {sport: "Футбол", champion: "Ліонель Мессі"},
+    {sport: "Формула-1", champion: "Макс Ферстаппен"},
+    {sport: "Бокс", champion: "Олександр Усик"}
+];
 
-const country = capitalAndCountry.get(cityOfResidence);
-const champion = sportAndChampion.get(favoriteSport);
+const country = capitalAndCountry.find((capital) => capital.capital === cityOfResidence);
+const champion = sportAndChampion.find((sport) => sport.sport === favoriteSport);
 
 const message = `${yearOfBirth !== null ? `Ваш вік: ${date.getFullYear() - Number(yearOfBirth)}` : ""}
-${cityOfResidence !== null ? country !== undefined ? `Ти живеш у столиці ${country}` : `Ти живеш у місті ${cityOfResidence}` : ""} 
-${champion !== undefined ? `Круто! Хочеш стати ${champion}?` : ""}`;
+${cityOfResidence !== null ? country !== undefined ? `Ти живеш у столиці ${country.country}` : `Ти живеш у місті ${cityOfResidence}` : ""} 
+${champion !== undefined ? `Круто! Хочеш стати ${champion.champion}?` : ""}`;
 
 if (message.match("[a-z0-9]+")) alert(message);
