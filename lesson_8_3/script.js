b527366a00e8d300ac31c4a0449a2bff46b1d29a
Honcharenko Danylo <danilo.gonch@gmail.com>
2025-10-15 12:52:22 +0300

    hm

---
+++ lesson_8_3/script.js
@@ -0,0 +1,12 @@
+function loop() {
+    let number;
+    let i = 1;
+    while (i <= 10) {
+        number = prompt("Введіть число більше 100");
+        if (number > 100) break;
+        i++;
+    }
+    console.log(number);
+}
+
+loop();
