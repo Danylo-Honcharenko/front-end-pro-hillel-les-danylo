f333e26cf8b0221277cce6624ce7b9d57fd81eb8
Honcharenko Danil <danilo.gonch@gmail.com>
2025-10-28 11:22:34 +0100

    refactor

--- lesson_11_3/script.js
+++ lesson_11_3/script.js
@@ -2,6 +2,6 @@ const photoBlock = document.querySelector("#photo-block");
 const imageNumber = Math.floor(Math.random() * 5) + 1;
 
 const image = document.createElement('img');
-image.setAttribute("src", `./photos/${imageNumber}.jpg`);
+image.src = `./photos/${imageNumber}.jpg`;
 
 photoBlock.appendChild(image);
