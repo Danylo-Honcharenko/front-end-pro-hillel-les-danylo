8e114976b8c314a73e00eee6c09984401b89ae23
Honcharenko Danil <danilo.gonch@gmail.com>
2025-12-14 13:22:21 +0200

    hm

---
+++ lesson_24/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import RequestFrom from "./components/RequestFrom.jsx";
+import ResponseBlock from "./components/ResponseBlock.jsx";
+
+function App() {
+
+  return (
+      <div className="container">
+        <RequestFrom />
+        <ResponseBlock />
+      </div>
+  )
+}
+
+export default App
---
+++ lesson_24/src/components/ResponseBlock.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ResponseBlock = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <span className="badge d-none bg-secondary js--swapi_controller">people</span>
+                <span className="badge d-none bg-secondary js--swapi_id">1</span>
+                <div className="load d-none js--swapi_load">
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+                <pre className="mt-2 mb-0 js--swapi_pre"><code></code></pre>
+            </div>
+        </div>
+    );
+};
+
+export default ResponseBlock;
