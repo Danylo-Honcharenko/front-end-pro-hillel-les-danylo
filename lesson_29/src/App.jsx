f4a3fbf729b2ff6b681d7c2b9487f023ee00c05c
Honcharenko Danil <danilo.gonch@gmail.com>
2026-01-05 17:15:03 +0200

    hm

---
+++ lesson_29/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Counter from "./components/Counter.jsx";
+import {useDispatch} from "react-redux";
+import {decrement, increment} from "./counterSlice.js";
+
+function App() {
+
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Counter />
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </>
+  );
+}
+
+export default App;
